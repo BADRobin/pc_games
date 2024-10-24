412f57c117c2b101b19702cb2f006c5230a24516
Herr Oleh Bryl <olegbryl@gmail.com>
2024-10-24 11:53:11 +0200

    start version3

--- src/main/java/dci/j24e01/pc_games/Category.java
+++ src/main/java/dci/j24e01/pc_games/Category.java
@@ -3,4 +3,33 @@ package dci.j24e01.pc_games;
 public class Category {
     private long id;
     private String category;
+
+    public Category(long id, String category) {
+        this.id = id;
+        this.category = category;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", category='" + category + '\'' +
+                '}';
+    }
 }
