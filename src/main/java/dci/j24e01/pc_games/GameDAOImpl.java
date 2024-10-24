5e7ff894dcb098b961f3892a42df6953d8f6cff7
Herr Oleh Bryl <olegbryl@gmail.com>
2024-10-24 14:44:42 +0200

    start version5

---
+++ src/main/java/dci/j24e01/pc_games/GameDAOImpl.java
@@ -0,0 +1,41 @@
+package dci.j24e01.pc_games;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameDAOImpl implements GameDAO {
+    private final Connection connection;
+
+    public GameDAOImpl(DBConnectionManager connectionManager) {
+        this.connection = connectionManager.getConnection();
+    }
+
+    @Override
+    public List<Game> getGames() {
+        String sql = "SELECT * FROM pc_games";
+//        try {
+//            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+//
+//            ResultSet resultSet = preparedStatement.executeQuery();
+//            System.out.println(resultSet);
+//
+//            List<Game> games = new ArrayList<>();
+//            while (resultSet.next()) {
+//                Game game = new Game(
+////                        resultSet.getLong("id"),
+////                        resultSet.getString("name"),
+////                        resultSet.getString("logo")
+//                );
+//                games.add(game);
+//            }
+//            return games;
+//        } catch(SQLException e) {
+//            e.printStackTrace();
+//            return null;
+//        }
+    }
+}
